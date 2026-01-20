58ea236a6d0a5ad7c73258f77aec564fb7feba4f
krishnabhargawa9-cyber <krishnabhargawa9@gmail.com>
2026-01-20 17:25:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const acc = document.querySelectorAll(".accordion");
-acc.forEach(btn => {
-  btn.addEventListener("click", () => {
-    const panel = btn.nextElementSibling;
-    panel.style.display = panel.style.display === "block" ? "none" : "block";
-  });
-});
